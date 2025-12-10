421d8dd853e500208db313fa7968343454ba26d5
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2025-12-10 01:27:09 -0500

    started implementing Reserve Service Option

--- controllers/serviceOptionsController.js
+++ controllers/serviceOptionsController.js
@@ -1,11 +1,29 @@
 const pool = require("../db");
 const { sendError, sendSuccess } = require("../utils/apiResponse");
 
+const service_options_table = `"quickcart".service_options`;
+
 async function pickupServiceOptions(req, res) {
   const { store_id } = req.body;
   if (!store_id) {
     sendError(res, `Field Missing store_id`, 400);
   }
+
+  let sql = `SELECT * FROM ${service_options_table} WHERE store_id = $1 AND available=$2`;
+  let values = [store_id, true];
+  let response = await pool.query(sql, values);
+  console.log(response.rows);
+  sendSuccess(res, null, response.rows, 200);
 }
 
-module.exports = pickupServiceOptions;
+async function reserveServiceoption(req, res) {
+  const { service_option_id } = req.body;
+
+  let sql = `UPDATE ${service_options_table} SET available=$1 WHERE service_option_id = $2`;
+  let values = [false, service_option_id];
+
+  let response = await pool.query(sql, values);
+  sendSuccess(res, "Reserved", null, 200);
+}
+
+module.exports = { pickupServiceOptions, reserveServiceoption };
--- routes/serviceOptionsRoutes.js
+++ routes/serviceOptionsRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const pickupServiceOptions = require("../controllers/serviceOptionsController");
+const {
+  pickupServiceOptions,
+  reserveServiceoption,
+} = require("../controllers/serviceOptionsController");
 
 router.post("/pickup", pickupServiceOptions);
+router.post("/reserve", reserveServiceoption);
 
 module.exports = router;
