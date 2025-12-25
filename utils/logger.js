f7bdabd0ee3d5b586472cb323356ebbb6faa1ac0
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2025-12-25 14:59:54 -0500

    ENG-8/ Transporting logs to DD

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,16 @@
 const pino = require("pino");
+const path = require("path");
 
-const logger = pino({
-  level: "debug",
-  base: {
-    service: process.env.SERVICE_NAME,
-    env: process.env.ENVIRONMENT,
+const logger = pino(
+  {
+    level: "debug",
+    base: {
+      service: process.env.SERVICE_NAME,
+      env: process.env.ENVIRONMENT,
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
   },
-  timestamp: pino.stdTimeFunctions.isoTime,
-});
+  pino.destination(path.join(__dirname, "../logs/quickcart.log"))
+);
 
 module.exports = logger;
