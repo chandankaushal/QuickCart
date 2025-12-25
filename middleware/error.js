64247bfc6016f1a3c9c16fb9f25ef67b2f5eaeea
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2025-12-25 15:12:56 -0500

    Added details to errorHandler for DataDog

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,25 @@
 const { ExpressError } = require("../utils/ExpressError");
+const logger = require("../utils/logger");
 
 function errorHandler(err, req, res, next) {
   const isExpressError = err instanceof ExpressError;
 
   const status = isExpressError ? err.statusCode : 500;
-  const message = err.message; // Remains same for all errors
+  const message = err.message ? err.message : "SERVER ERROR"; // Remains same for all errors
   const code = isExpressError ? err.code : "INTERNAL_ERROR";
+
+  logger.error(
+    {
+      error: message,
+      code: code,
+      statusCode: status,
+      method: req.method,
+      path: req.path,
+      userId: req.user?.id,
+      stack: err.stack,
+    },
+    "There was an error in the request"
+  );
 
   res.status(status).json({ status: "error", message, code });
 }
