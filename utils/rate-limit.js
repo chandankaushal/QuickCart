c4c8422f6952d2803cbb87c71d4201a8848796d1
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2026-01-09 20:52:47 -0500

    express error

--- utils/rate-limit.js
+++ utils/rate-limit.js
@@ -1,15 +1,17 @@
 const { rateLimit } = require("express-rate-limit");
+const { ExpressError } = require("./ExpressError");
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  limit: 10, // Limit each IP to 100 requests per `window`
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-  message: {
-    status: "error",
-    message: "Too many requests",
-    code: "RATE_LIMIT_EXCEEDED",
+  windowMs: 1 * 60 * 1000,
+  limit: 1,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  handler: (req, res, next, options) => {
+    res.status(429).json({
+      status: "error",
+      message: "Too many requests",
+      code: "RATE_LIMIT_EXCEEDED",
+    });
   },
 });
 
