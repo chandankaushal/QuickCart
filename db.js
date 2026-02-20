956528d1f0bb381506c5e92e96cb881b04371de0
chandankaushal <137195689+chandankaushal@users.noreply.github.com>
2026-02-20 08:35:30 -0500

    cleaning up

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
