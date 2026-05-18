7fee67b1807c713bfb6c6b4b519e0d937bcb28cf
Chandan Kaushal <chandankaushal7999@gmail.com>
2026-05-18 00:59:01 -0400

    implement getAvailableProducts

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const { checkProductAvailabilty } = require("../controllers/productController");
+const {
+  checkProductAvailabilty,
+  getAvailableProducts,
+} = require("../controllers/productController");
+const { checkValidToken } = require("../middleware/auth");
+const { getAvailableProductsSchema } = require("../models/joiSchema");
+const { validateBody } = require("../middleware/validate");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.post("/checkAvailability", checkProductAvailabilty);
+router.post(
+  "/available",
+  validateBody(getAvailableProductsSchema),
+  checkValidToken,
+  wrapAsync(getAvailableProducts),
+);
 // router.post("/create", productController);
 // router.post("/updateAvailabilty", updateAvailabilty); //just to test not needed in real app
 // router.delete("/deleteProduct", productController);
